dc859ea5807aec1ad053b356b73000e100c4bff9
Mayank Agrawal <amayank@umich.edu>
2021-02-11 23:50:26 -0600

    hydration_profile now accepts itpfiles instead of a single itpfile. Works for multi molecule assemblies

--- PAanalysis/hydration_profile.py
+++ PAanalysis/hydration_profile.py
@@ -9,7 +9,7 @@ import mdtraj
 
 
 
-def hydration_profile(itpfile, topfile, grofile, trajfile, radius, frame_iterator):
+def hydration_profile(itpfiles, topfile, grofile, trajfile, radius, frame_iterator, whichitp=0):
     """For the given MARTINI system files, 
     calculates the water density per MARTINI residue within the shell of radius <radius>
     relative to the global water density.
@@ -17,50 +17,83 @@ def hydration_profile(itpfile, topfile, grofile, trajfile, radius, frame_iterato
     Returns: residue names, hydration profile, global water density
 
     trajfile: <.trr/.xtc>
+    itpfiles should be in order of how the corresponding molecules appear in the grofile
+    whichitp is the index of the molecules itp in <itpfiles> that will be used to calculate hydration on
     """
 
+    itpnames = []
+    bb_bonds_permols = []
+    num_atomss    = []
+    nmols         = []
+
+    for itpfile in itpfiles:
+        itpname = os.path.basename(itpfile).strip('.itp')
+        itpnames += [itpname]
+        bb_bonds_permols += [get_backbone_bonds_permol(itpfile)]
+        num_atomss    += [get_num_atoms(itpfile)]
+        nmols         += [get_num_molecules(topfile, itpname)]
     
-    itpname = os.path.basename(itpfile).strip('.itp')
-    bb_bonds_permol = get_backbone_bonds_permol(itpfile)
-    num_atoms    = get_num_atoms(itpfile)
-    nmol         = get_num_molecules(topfile, itpname)
-    start_index  = 0
-    positions    = get_positions(grofile, trajfile, (start_index, num_atoms*nmol))
-    num_frames = positions.shape[0]
-    positions = positions.reshape(-1,nmol,num_atoms,3)
-    shape = positions.shape
+    
+    start_index = 0
+    num_atoms_ = 0
+    positionss = []
+    for i in range(len(itpfiles)):
+        num_atoms_ += num_atomss[i]*nmols[i]
+        positionss += [get_positions(grofile, trajfile, (start_index, num_atoms_))]
+        start_index = num_atoms_
+    
     nmol_W         = get_num_molecules(topfile, 'W')
-    positions_W    = get_positions(grofile, trajfile, (num_atoms*nmol, num_atoms*nmol+nmol_W))
+    positions_W    = get_positions(grofile, trajfile, (start_index, start_index+nmol_W))
     
     # Get Lx, Ly, Lz
-    traj = md.load(trajfile, top=grofile)
+    traj = mdtraj.load(trajfile, top=grofile)
     Lx, Ly, Lz = traj.unitcell_lengths[-1]
     box = dict(Lx=Lx, Ly=Ly, Lz=Lz)
 
     # atom indices from itpfile
-    pep_indices = []
-    PAM_indices = []
-    res_names = []
-    start = False
-    with open(itpfile, 'r') as f:
-        for line in f:
-            if '[ atoms ]' in line:
-                start = True
-                continue
-            if start:
-                words = line.split()
-                if words == []:
-                    break
-                if words[3] == 'PAM':
-                    PAM_indices += [int(words[0])-1]
-                else:
-                    pep_indices += [int(words[0])-1]
-                    res_names += [words[3]+'\n'+words[4]]
+    PAM_indicess = []
+    res_namess = []
+    pep_indicess = []
+    for itpfile in itpfiles:
+        pep_indices = []
+        PAM_indices = []
+        res_names = []
+        start = False
+        with open(itpfile, 'r') as f:
+            for line in f:
+                if '[ atoms ]' in line:
+                    start = True
+                    continue
+                if start:
+                    words = line.split()
+                    if words == []:
+                        break
+                    if words[3] == 'PAM':
+                        PAM_indices += [int(words[0])-1]
+                    else:
+                        pep_indices += [int(words[0])-1]
+                        res_names += [words[3]+'\n'+words[4]]
 
-    # reverse indices of PAM
+        # reverse indices of PAM
+        atom_indices = PAM_indices[::-1] + pep_indices
+        res_names = ['PAM']*len(PAM_indices) + res_names
+        
+        PAM_indicess += [PAM_indices]
+        res_namess += [res_names]
+        pep_indicess += [pep_indices]
+
+    # selection for a particular itp <whichitp>
+    positions = positionss[whichitp]
+    nmol = nmols[whichitp]
+    num_atoms = num_atomss[whichitp]
+    positions = positions.reshape(-1,nmol,num_atoms,3)
+    shape = positions.shape
+    PAM_indices = PAM_indicess[whichitp]
+    res_names = res_namess[whichitp]
+    pep_indices = pep_indicess[whichitp]
     atom_indices = PAM_indices[::-1] + pep_indices
-    res_names = ['PAM']*len(PAM_indices) + res_names
-
+    
+    
     # calculate hydration profile
     global_water_density = nmol_W / (Lx*Ly*Lz)
 
