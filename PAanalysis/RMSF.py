cf230db4d8a49d68c3f203e675f209f424ba8452
Mayank Agrawal <amayank@umich.edu>
2021-10-21 17:56:53 -0500

    rsmf works well now: calculates rmsf after rotating each frame such that gyration vectors points towards cartesian. The head and tail of gyration vecs, when calculated, are random. This rectified by using the orientation is closest to the orientation of the gyration vector in the previous frame.

--- PAanalysis/RMSF.py
+++ PAanalysis/RMSF.py
@@ -25,7 +25,7 @@ def RMSF(grofile, trajfile, frame_iterator):
     
     num_frames = traj.n_frames
     
-    #------------------------------------ Backbone args ------------------------
+    #------------------------------------ Non-water atoms ------------------------
     backbone_args = []
     not_water_args = []
     for atom in traj.top.atoms:
@@ -42,51 +42,55 @@ def RMSF(grofile, trajfile, frame_iterator):
 
     #------------------------------------ Box Images ------------------------
     # Identify images if particles jump more than half the box length
-    
     unitcell_lengths = [traj.unitcell_lengths[f] for f in range(num_frames)]
     images = utils.find_box_images(positions[:,args], unitcell_lengths)
-
     #------------------------------------------------------------------------
-
-    #--------------------- Make orientational and translation invarient -------------------
     
-    points = []
-    for f in frame_iterator:
+    points = positions[frame_iterator][:,args]
+
+    #-------------------------- Unwrap and centralize -----------------------
+    for n,f in enumerate(frame_iterator):
         Lx, Ly, Lz = traj.unitcell_lengths[f]
         box = freud.box.Box(Lx=Lx, Ly=Ly, Lz=Lz, is2D=False)
-
-        points_f = positions[f, args]
+        points_f = points[n]
         points_f -= [Lx/2, Ly/2, Lz/2]
         points_f = box.unwrap(points_f, images[f])
         points_f -= np.mean(points_f, axis=0)
+        points[n] = points_f
+
+    #------------------------------------------------------------------------
+
+    #---------------------- Make orientational invariant --------------------
+    for n,f in enumerate(frame_iterator):
+        points_f = np.copy(points[n])
         
+        # gyration vectors
         w,eigvec = utils.gyration(points_f)
         w = np.abs(w)
         wargs = np.argsort(w)[::-1]
         w = w[wargs]
         eigvec = eigvec[:,wargs]
-        
-        for i,v in enumerate([[1,0,0],[0,1,0],[0,0,1]]):
-            q = quaternion.q_between_vectors(eigvec[:,i], v)
-            for j,p in enumerate(points_f):
-                points_f[j] = quaternion.qv_mult(q,p)
-            # also rotate rest of the eigenvectors
-            for j in range(i+1,3):
-                eigvec[:,j] = quaternion.qv_mult(q,eigvec[:,j])            
+        if n!=0: # flip eigvec near to eigvec_prev
+            dotproducts = np.sum( eigvec * eigvec_prev, axis=0, keepdims=True )
+            eigvec *= np.sign(dotproducts)
+        eigvec_prev = np.copy(eigvec)
 
-        points += [ points_f ]
+        # rotate system towards cartesian
+        v = eigvec[:,0]
+        qx = quaternion.q_between_vectors( v, [1,0,0] )
+        v = quaternion.qv_mult( qx, eigvec[:,1] )
+        qy = quaternion.q_between_vectors( v, [0,1,0] )
+        q = quaternion.qq_mult( qy, qx )
+        v = quaternion.qv_mult( q, eigvec[:,2] )
+        qz = quaternion.q_between_vectors( v, [0,0,1] )
+        q = quaternion.qq_mult( qz, q )
+        points_f = [ quaternion.qv_mult(q,p) for p in points_f]
         
-        # w,eigvec = utils.gyration(points_f)
-        # w = np.real(w)
-        # wargs = np.argsort(w)[::-1]
-        # w = w[wargs]
-        # eigvec = eigvec[:,wargs]
-        # print(eigvec)
-    
-    points = np.array(points)
-    
+        points[n] = points_f
+    #------------------------------------------------------------------------
+
+    # Calculate RMSF
     points_mean = np.mean(points, axis=0, keepdims=True)
-
     rmsf = np.sqrt(np.mean((points - points_mean)**2))
 
     return rmsf
