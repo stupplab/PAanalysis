42e3294007c61f126b2789f641bf3041784add11
Mayank Agrawal <amayank@umich.edu>
2021-10-22 22:24:22 -0500

    r_O_H calculation is reduced

--- PAanalysis/vibrational_spectra.py
+++ PAanalysis/vibrational_spectra.py
@@ -196,10 +196,10 @@ def r_O_H(grofile, trajfile, frame_iterator, topfile, molname, filenamerdf=None,
     """Calculates avearge distance between O (C=O) and H (NH) and H (HOH)
     r_CO_HN
     r_CO_HOH
-    
+
     r is calculated af the average distance of O (C=O) from the nearest H (NH / HOH)
     """
-    
+
     #---------------------------------------------------------------------------------
 
     traj = mdtraj.load(trajfile, top=grofile)
@@ -311,6 +311,9 @@ def r_O_H(grofile, trajfile, frame_iterator, topfile, molname, filenamerdf=None,
 
     r_CO_NH_mean = np.mean(r_CO_NH)
     r_CO_HOH_mean = np.mean(r_CO_HOH)
+
+
+    return r_CO_NH_mean, r_CO_HOH_mean
 
     #---------------------------------------------------------------------------------
 
