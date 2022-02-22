dddfe476b55a967f083e78b0e3303d8ab9033da8
Mayank Agrawal <amayank@umich.edu>
2022-02-22 15:25:25 -0600

    RMSF specific residues, make_orientation_invariante made optional

--- PAanalysis/RMSF.py
+++ PAanalysis/RMSF.py
@@ -98,7 +98,7 @@ def RMSF(grofile, trajfile, frame_iterator, make_orientation_invariate=True):
 
 
 
-def RMSF_specific_residues(grofile, trajfile, frame_iterator, residuenames):
+def RMSF_specific_residues(grofile, trajfile, frame_iterator, residuenames, make_orientation_invariate=True):
     """
     atomistic simulation
     Root mean square fluctuation of the peptide backbone
@@ -153,32 +153,33 @@ def RMSF_specific_residues(grofile, trajfile, frame_iterator, residuenames):
     #------------------------------------------------------------------------
 
     #---------------------- Make orientational invariant --------------------
-    for n,f in enumerate(frame_iterator):
-        points_f = np.copy(points[n])
-        
-        # gyration vectors
-        w,eigvec = utils.gyration(points_f)
-        w = np.abs(w)
-        wargs = np.argsort(w)[::-1]
-        w = w[wargs]
-        eigvec = eigvec[:,wargs]
-        if n!=0: # flip eigvec near to eigvec_prev
-            dotproducts = np.sum( eigvec * eigvec_prev, axis=0, keepdims=True )
-            eigvec *= np.sign(dotproducts)
-        eigvec_prev = np.copy(eigvec)
+    if make_orientation_invariate:
+        for n,f in enumerate(frame_iterator):
+            points_f = np.copy(points[n])
+            
+            # gyration vectors
+            w,eigvec = utils.gyration(points_f)
+            w = np.abs(w)
+            wargs = np.argsort(w)[::-1]
+            w = w[wargs]
+            eigvec = eigvec[:,wargs]
+            if n!=0: # flip eigvec near to eigvec_prev
+                dotproducts = np.sum( eigvec * eigvec_prev, axis=0, keepdims=True )
+                eigvec *= np.sign(dotproducts)
+            eigvec_prev = np.copy(eigvec)
 
-        # rotate system towards cartesian
-        v = eigvec[:,0]
-        qx = quaternion.q_between_vectors( v, [1,0,0] )
-        v = quaternion.qv_mult( qx, eigvec[:,1] )
-        qy = quaternion.q_between_vectors( v, [0,1,0] )
-        q = quaternion.qq_mult( qy, qx )
-        v = quaternion.qv_mult( q, eigvec[:,2] )
-        qz = quaternion.q_between_vectors( v, [0,0,1] )
-        q = quaternion.qq_mult( qz, q )
-        points_f = [ quaternion.qv_mult(q,p) for p in points_f]
-        
-        points[n] = points_f
+            # rotate system towards cartesian
+            v = eigvec[:,0]
+            qx = quaternion.q_between_vectors( v, [1,0,0] )
+            v = quaternion.qv_mult( qx, eigvec[:,1] )
+            qy = quaternion.q_between_vectors( v, [0,1,0] )
+            q = quaternion.qq_mult( qy, qx )
+            v = quaternion.qv_mult( q, eigvec[:,2] )
+            qz = quaternion.q_between_vectors( v, [0,0,1] )
+            q = quaternion.qq_mult( qz, q )
+            points_f = [ quaternion.qv_mult(q,p) for p in points_f]
+            
+            points[n] = points_f
     #------------------------------------------------------------------------
 
     # Calculate RMSF
